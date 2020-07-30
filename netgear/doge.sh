5e6013b29d964dc85052138c3d0f75fa91ecce8e
iamgayasfuck <leptichaun72@github.com>
2020-07-30 01:33:38 -0400

    Integrate doge.sh into knife.sh

--- netgear/doge.sh
+++
@@ -1,3 +0,0 @@
-export PATH=$PATH:/media
-alias bb="busybox-armv5l"
-
--- netgear/knife.sh
+++ netgear/knife.sh
@@ -2,7 +2,7 @@
 ## ----------------------------------
 # Step #1: Define variables
 # ----------------------------------
-#EDITOR=vim
+#EDITOR=vi
 RED='\033[0;41;30m'
 STD='\033[0;0;39m'
  
@@ -13,49 +13,53 @@ pause(){
   read -p "Press [Enter] key to continue..." fackEnterKey
 }
 one(){
-	echo "one() called"
-        pause
+  echo "Setting the environment..."
+  export PATH=$PATH:/media
+  alias bb="busybox-armv5l"
+
+  pause
 }
 two(){
-	echo "two() called"
-        pause
+  echo "Started file server..."
+  /media/busybox-armv5l tcpsvd -vE 0.0.0.0 21 /media/busybox-armv5l ftpd -w /media
+  
+  pause
 }
- 
+
 # function to display menus
 show_menus() {
-	echo "~~~~~~~~~~~~~~~~~~~~~"	
-	echo " M A I N - M E N U"
-	echo "~~~~~~~~~~~~~~~~~~~~~"
-	echo "1. Set Terminal"
-	echo "2. Reset Terminal"
-	echo "3. Exit"
+  echo "~~~~~~~~~~~~~~~~~~~~~"	
+  echo " M A I N - M E N U"
+  echo "~~~~~~~~~~~~~~~~~~~~~"
+  echo "1. Setup environment"
+  echo "2. Start FTP daemon"
+  echo "3. Exit"
 }
 # read input from the keyboard and take a action
 # invoke the one() when the user select 1 from the menu option.
 # invoke the two() when the user select 2 from the menu option.
 # Exit when user the user select 3 form the menu option.
 read_options(){
-	local choice
-	read -p "Enter choice [ 1 - 3] " choice
-	case $choice in
-		1) one ;;
-		2) two ;;
-		3) exit 0;;
-		*) echo -e "${RED}Error...${STD}" && sleep 2
-	esac
+  local choice
+  read -p "Enter choice [ 1 - 3] " choice
+  case $choice in
+    1) one ;;
+    2) two ;;
+    3) break;;
+    *) echo -e "${RED}Error...${STD}" && sleep 2
+  esac
 }
  
 # ----------------------------------------------
 # Step #3: Trap CTRL+C, CTRL+Z and quit singles
 # ----------------------------------------------
-trap '' SIGINT SIGQUIT SIGTSTP
+#trap '' SIGINT SIGQUIT SIGTSTP
  
 # -----------------------------------
 # Step #4: Main logic - infinite loop
 # ------------------------------------
 while :
 do
- 
-	show_menus
-	read_options
+  show_menus
+  read_options
 done
